ab12baf251c00ba91b07f1bd5bc2ea2d349eb018
KoyaSaeki <koyasaeki@gmail.com>
2022-07-09 18:55:10 +0900

    ✨: Setup styled-dictionary.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# design-token
+# Design Token
---
+++ build/ios/StyleDictionaryColor.h
@@ -0,0 +1,26 @@
+
+//
+// StyleDictionaryColor.h
+//
+
+// Do not edit directly
+// Generated on Sat, 09 Jul 2022 09:50:23 GMT
+
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorBaseGrayLight,
+ColorBaseGrayMedium,
+ColorBaseGrayDark,
+ColorBaseRed,
+ColorBaseGreen,
+ColorFontBase,
+ColorFontSecondary,
+ColorFontTertiary
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
